917618c9f43581bbff15d836ff67455f41d510a0
diogoferla <diogoferla@gmail.com>
2026-09-12 09:39:42 -0300

    Delete supabase-config.js

--- supabase-config.js
+++
@@ -1,5 +0,0 @@
-// CONFIGURAÇÃO DO SUPABASE
-// Cole aqui somente a URL do projeto e a PUBLISHABLE KEY.
-// NUNCA coloque a service_role/secret key neste arquivo.
-const SUPABASE_URL = 'https://yngpwflmlipthvgkfbue.supabase.co';
-const SUPABASE_PUBLISHABLE_KEY = 'sb_publishable_jm_plPj3xmnhE7I5OthulQ_NDtpCSeu';
