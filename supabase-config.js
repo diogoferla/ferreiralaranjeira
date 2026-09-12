0101974460893edad2748804bc8e26c662cac364
diogoferla <diogoferla@gmail.com>
2026-09-11 22:48:18 -0300

    Add files via upload

---
+++ supabase-config.js
@@ -0,0 +1,5 @@
+// CONFIGURAÇÃO DO SUPABASE
+// Cole aqui somente a URL do projeto e a PUBLISHABLE KEY.
+// NUNCA coloque a service_role/secret key neste arquivo.
+const SUPABASE_URL = 'COLE_AQUI_A_URL_DO_SEU_PROJETO';
+const SUPABASE_PUBLISHABLE_KEY = 'COLE_AQUI_A_PUBLISHABLE_KEY';
